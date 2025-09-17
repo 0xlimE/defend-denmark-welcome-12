d0c80aa9b638900ef23643e848bd08351eded078
0xlimE <emilhoerning@gmail.com>
2025-09-17 20:07:02 +0200

    LETS GO BUILD

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/defend-denmark-welcome-12/',
+  base: mode === 'production' ? '/defend-denmark-welcome-12/' : '/',
   server: {
     host: "::",
     port: 8080,
